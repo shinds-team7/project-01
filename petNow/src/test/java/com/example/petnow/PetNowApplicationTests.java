20ab0b75856a7d4233b04a2309720be36da80c7f
hsbae <t01035724710@gmail.com>
2026-07-27 17:04:29 +0900

    자바초기세팅

---
+++ petNow/src/test/java/com/example/petnow/PetNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetNowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
