bf54e04a36ca848fa14a6a2133d2fe0563947ad3
rhkrqkq <rhkrqkq1025@gmail.com>
2026-07-27 17:19:00 +0900

    전역 예외 처리

---
+++ petNow/src/main/java/com/example/petnow/dto/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.example.petnow.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ErrorResponse {
+
+	private String code;
+	private String message;
+}
+
