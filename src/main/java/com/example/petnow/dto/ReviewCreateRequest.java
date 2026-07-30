347390af651af19aac4dec0685f04ff76e3e0365
yunsy <yunsy0119@gmail.com>
2026-07-30 16:48:56 +0900

    feat: 리뷰 작성 요청 DTO 제약사항 추가

--- src/main/java/com/example/petnow/dto/ReviewCreateRequest.java
+++ src/main/java/com/example/petnow/dto/ReviewCreateRequest.java
@@ -1,5 +1,9 @@
 package com.example.petnow.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -8,7 +12,14 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class ReviewCreateRequest {
+    @NotNull(message = "평점은 필수입니다.")
+    @Min(value = 1, message = "평점은 1점 이상이어야 합니다.")
+    @Max(value = 5, message = "평점은 5점 이하여야 합니다.")
     private Integer score;
+
+    @Size(max = 50, message = "리뷰 내용은 50자 이하여야 합니다.")
     private String comment;
+
+    @NotNull(message = "예약 ID는 필수입니다.")
     private Long reservationId;
 }
