f5e2f3a87e6a31e744194e367df0337ec81ffe45
seon0 <tjsdud00sy@gmail.com>
2026-07-30 16:45:38 +0900

    feat: 로그인 controller 추가구현

--- src/main/java/com/example/petnow/controller/UserController.java
+++ src/main/java/com/example/petnow/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.example.petnow.controller;
 
-import com.example.petnow.dto.UserSignupRequest;
+import com.example.petnow.dto.request.UserLoginRequest;
+import com.example.petnow.dto.request.UserSignupRequest;
 import com.example.petnow.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -15,9 +14,17 @@ public class UserController {
 
     private final UserService userService;
 
+    // 회원가입
     @PostMapping("/signup")
     public String addUser(@RequestBody UserSignupRequest request) {
         userService.signup(request);
+        return "mypage";
+    }
+
+    // 로그인
+    @PostMapping ("/login")
+    public String userLogin(@RequestBody UserLoginRequest request) {
+        userService.login(request);
         return "mypage";
     }
 }
