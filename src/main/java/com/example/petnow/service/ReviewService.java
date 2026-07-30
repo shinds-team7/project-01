9f829e62d5dc074d91f5e29e79703c363a13bad2
yunsy <yunsy0119@gmail.com>
2026-07-30 16:49:41 +0900

    feat: 리뷰 작성 서비스 트랜잭션 처리

--- src/main/java/com/example/petnow/service/ReviewService.java
+++ src/main/java/com/example/petnow/service/ReviewService.java
@@ -5,6 +5,7 @@ import com.example.petnow.entity.Review;
 import com.example.petnow.mapper.ReviewMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +13,7 @@ public class ReviewService {
 
     private final ReviewMapper reviewMapper;
 
+    @Transactional
     public void insertReview(ReviewCreateRequest request) {
         Review review = Review.builder()
                 .score(request.getScore())
