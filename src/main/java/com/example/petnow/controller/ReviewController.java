8774397ec735db50a7db98b2babd0e86c4122d04
yunsy <yunsy0119@gmail.com>
2026-07-30 14:27:09 +0900

    feat: 리뷰 조회 컨트롤러 추가

--- src/main/java/com/example/petnow/controller/ReviewController.java
+++ src/main/java/com/example/petnow/controller/ReviewController.java
@@ -1,14 +1,13 @@
 package com.example.petnow.controller;
 
-import com.example.petnow.dto.ReviewCreateRequest;
-import com.example.petnow.entity.Review;
+import com.example.petnow.dto.request.ReviewCreateRequest;
+import com.example.petnow.dto.response.ReviewResponse;
 import com.example.petnow.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/reviews")
@@ -22,4 +21,17 @@ public class ReviewController {
         reviewService.insertReview(request);
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<List<ReviewResponse>> getMyReviews(@PathVariable Long userId) {
+
+        return ResponseEntity.ok(reviewService.getReviewsByUser(userId));
+    }
+
+    @GetMapping("/places/{placeId}")
+    public ResponseEntity<List<ReviewResponse>> getPlaceReviews(@PathVariable Long placeId) {
+
+        return ResponseEntity.ok(reviewService.getReviewsByPlace(placeId));
+    }
+
 }
