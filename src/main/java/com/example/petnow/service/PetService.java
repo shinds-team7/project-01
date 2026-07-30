f45148cf8d10c281f2a724fd20682ba90f9c6a84
kowonyeong <ganenode@naver.com>
2026-07-30 17:12:11 +0900

    fix: getPetList와 updatePet 서비스 구현

--- src/main/java/com/example/petnow/service/PetService.java
+++ src/main/java/com/example/petnow/service/PetService.java
@@ -1,8 +1,13 @@
 package com.example.petnow.service;
 
 import com.example.petnow.dto.request.PetCreateRequest;
+import com.example.petnow.dto.request.PetUpdateRequest;
+import com.example.petnow.dto.response.PetListResponse;
+
+import java.util.List;
 
 public interface PetService {
     void createPet(Long userId, PetCreateRequest request);
-
+    List<PetListResponse> getPetList(Long userId);
+    void updatePet(Long petId, PetUpdateRequest request);
 }
--- src/main/java/com/example/petnow/service/PetServiceImpl.java
+++ src/main/java/com/example/petnow/service/PetServiceImpl.java
@@ -1,13 +1,16 @@
 package com.example.petnow.service;
 
 import com.example.petnow.dto.request.PetCreateRequest;
+import com.example.petnow.dto.request.PetUpdateRequest;
+import com.example.petnow.dto.response.PetListResponse;
 import com.example.petnow.entity.Pet;
-import com.example.petnow.entity.PetPhoto;
 import com.example.petnow.mapper.PetMapper;
 import com.example.petnow.mapper.PetPhotoMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -34,5 +37,18 @@ public class PetServiceImpl implements PetService {
 
         // pets 테이블 저장
         petMapper.insertPet(pet);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<PetListResponse> getPetList(Long userId) {
+
+        return petMapper.getPetList(userId);
+    }
+
+    @Override
+    public void updatePet(Long petId, PetUpdateRequest request){
+        request.setPetId(petId);
+        petMapper.updatePet(request);
     }
 }
