9e27715f57a703d81be5fd6c47cb7ff91c861e80
kowonyeong <ganenode@naver.com>
2026-07-30 16:57:48 +0900

    feat: 반려동물 정보 수정 api구현

--- src/main/java/com/example/petnow/controller/PetController.java
+++ src/main/java/com/example/petnow/controller/PetController.java
@@ -1,24 +1,27 @@
 package com.example.petnow.controller;
 
 import com.example.petnow.dto.request.PetCreateRequest;
+import com.example.petnow.dto.request.PetUpdateRequest;
 import com.example.petnow.service.PetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/pet")
 @RequiredArgsConstructor
 public class PetController {
     private final PetService petService;
 
-    @PostMapping("/addpet")
-    public String addpet(@RequestBody PetCreateRequest request){
+    @PostMapping("/create")
+    public String addPet(@RequestBody PetCreateRequest createRequest){
+        petService.createPet(1L, createRequest);
+        return "mypage";
+    }
 
-        petService.createPet(1L,request);
+    @PostMapping("/update")
+    public String updatePet(@RequestBody PetUpdateRequest updateRequest){
+        petService.updatePet(3L,updateRequest);
         return "mypage";
     }
 }
