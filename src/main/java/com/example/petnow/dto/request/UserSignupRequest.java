5fdef7ffe21eb3f4729cb81f57b776938f071881
seon0 <tjsdud00sy@gmail.com>
2026-08-01 02:47:58 +0900

    feat: 회원가입 요청값 검증 추가

--- src/main/java/com/example/petnow/dto/request/UserSignupRequest.java
+++ src/main/java/com/example/petnow/dto/request/UserSignupRequest.java
@@ -1,13 +1,23 @@
 package com.example.petnow.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class UserSignupRequest {
 
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "올비른 이메일 형식이 아닙니다.")
     private String email;
+
+    @NotBlank(message = "닉네임을 입력해주세요.")
     private String nickname;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 }
