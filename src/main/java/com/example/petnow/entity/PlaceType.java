47ba0dcb78442f0d87bfd8c3a04653bee2f673fa
hsbae <t01035724710@gmail.com>
2026-07-31 12:13:59 +0900

    feat: Place type entity 구현

--- src/main/java/com/example/petnow/entity/PlaceType.java
+++ src/main/java/com/example/petnow/entity/PlaceType.java
@@ -1,7 +1,14 @@
 package com.example.petnow.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum PlaceType {
-    APARTMENT,
-    HOUSE,
-    OFFICETEL,
+    APARTMENT("아파트"),
+    HOUSE("주택"),
+    OFFICETEL("오피스텔");
+
+    private final String label;
 }
