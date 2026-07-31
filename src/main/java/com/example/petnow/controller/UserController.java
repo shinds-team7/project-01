ab1c37d4e5d2db47464d7c1112d90c05c2f5dc64
seon0 <tjsdud00sy@gmail.com>
2026-08-01 02:55:16 +0900

    refactor: 회원가입·로그인 요청 @RequestBody -> @ModelAttribute로 변경

--- src/main/java/com/example/petnow/controller/UserController.java
+++ src/main/java/com/example/petnow/controller/UserController.java
@@ -3,9 +3,13 @@ package com.example.petnow.controller;
 import com.example.petnow.dto.request.UserLoginRequest;
 import com.example.petnow.dto.request.UserSignupRequest;
 import com.example.petnow.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
@@ -14,16 +18,16 @@ public class UserController {
 
     private final UserService userService;
 
-    // 회원가입
+    // 회원가입 , 메인페이지 생성 되면 return을 메인페이지로 바꿀 예정
     @PostMapping("/signup")
-    public String addUser(@RequestBody UserSignupRequest request) {
+    public String userSignup(@Valid @ModelAttribute UserSignupRequest request) {
         userService.signup(request);
         return "mypage";
     }
 
-    // 로그인
+    // 로그인 , 메인페이지 생성 되면 return을 메인페이지로 바꿀 예정
     @PostMapping ("/login")
-    public String userLogin(@RequestBody UserLoginRequest request) {
+    public String userLogin(@Valid @ModelAttribute UserLoginRequest request) {
         userService.login(request);
         return "mypage";
     }
