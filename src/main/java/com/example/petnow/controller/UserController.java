29d1dcb981c3acfcf5a3bf2627c48d84f82692f9
seon0 <tjsdud00sy@gmail.com>
2026-08-03 19:35:30 +0900

    feat: 로그인 성공 시 유저ID 세션 저장

--- src/main/java/com/example/petnow/controller/UserController.java
+++ src/main/java/com/example/petnow/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.petnow.controller;
 import com.example.petnow.dto.request.UserLoginRequest;
 import com.example.petnow.dto.request.UserSignupRequest;
 import com.example.petnow.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -27,8 +28,13 @@ public class UserController {
 
     // 로그인 , 메인페이지 생성 되면 return을 메인페이지로 바꿀 예정
     @PostMapping ("/login")
-    public String userLogin(@Valid @ModelAttribute UserLoginRequest request) {
-        userService.login(request);
+    public String userLogin(@Valid @ModelAttribute UserLoginRequest request,
+                            HttpSession session) {
+
+        Long userId = userService.login(request);
+
+        session.setAttribute("loginUserId", userId);
+
         return "mypage";
     }
 }
