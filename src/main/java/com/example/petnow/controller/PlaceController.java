e541c88ec5c888697719be7b66c6b5b649a1e534
hsbae <t01035724710@gmail.com>
2026-08-03 12:33:09 +0900

    feat: Place controller의 매개변수 DTO 규칙으로 인한 변경

--- src/main/java/com/example/petnow/controller/PlaceController.java
+++ src/main/java/com/example/petnow/controller/PlaceController.java
@@ -1,6 +1,6 @@
 package com.example.petnow.controller;
 
-import com.example.petnow.dto.request.PlaceCreateRequestDTO;
+import com.example.petnow.dto.request.PlaceCreateRequest;
 import com.example.petnow.entity.PlaceType;
 import com.example.petnow.service.PlaceService;
 import jakarta.validation.Valid;
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,8 +23,8 @@ public class PlaceController {
     @GetMapping("/create")
     public String createForm(Model model) {
         model.addAttribute(
-                "placeCreateRequestDTO",
-                new PlaceCreateRequestDTO()
+                "placeCreateRequest",
+                new PlaceCreateRequest()
         );
         addCreateFormAttributes(model);
 
@@ -32,7 +33,8 @@ public class PlaceController {
 
     //TODO: 로그인 구현 후 세션 사용자 ID로 교체
     @PostMapping
-    public String create(@Valid PlaceCreateRequestDTO requestDTO, BindingResult bindingResult,
+    public String create(@Valid @ModelAttribute("placeCreateRequest") PlaceCreateRequest requestDTO,
+                         BindingResult bindingResult,
                          Model model) {
         if (bindingResult.hasErrors()) {
             addCreateFormAttributes(model);
