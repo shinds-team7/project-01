67d46f5c41759e43a1046dfaed2c57b84fcd99dd
rhkrqkq <rhkrqkq1025@gmail.com>
2026-08-03 10:03:03 +0900

    refactor: 불필요 import문 제거

--- src/main/java/com/example/petnow/controller/ReservationController.java
+++ src/main/java/com/example/petnow/controller/ReservationController.java
@@ -1,8 +1,6 @@
 package com.example.petnow.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
