168d8ab99907acc7d94c58b24ed63ccbb6c34692
yunsy <yunsy0119@gmail.com>
2026-08-04 15:54:15 +0900

    fix: UserController 오타로 인한 오류 수정

--- src/main/java/com/example/petnow/controller/UserController.java
+++ src/main/java/com/example/petnow/controller/UserController.java
@@ -28,7 +28,7 @@ public class UserController {
 
     // 로그인 , 메인페이지 생성 되면 return을 메인페이지로 바꿀 예정
     @PostMapping ("/login")
-<<<<<<< HEAD
+
     public String userLogin(@Valid @ModelAttribute UserLoginRequest request, HttpSession session) {
 
         Long userId = userService.login(request);
