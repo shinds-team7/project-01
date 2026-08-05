067a3357a9feaac321651aa89b8d666caf5f3123
yunsy <yunsy0119@gmail.com>
2026-08-05 17:01:20 +0900

    feat: ReviewCreateRequest DTO 수정

--- src/main/java/com/example/petnow/dto/request/ReviewCreateRequest.java
+++ src/main/java/com/example/petnow/dto/request/ReviewCreateRequest.java
@@ -12,6 +12,10 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class ReviewCreateRequest {
+
+    @NotNull(message = "예약 ID는 필수입니다.")
+    private Long reservationId;
+
     @NotNull(message = "평점은 필수입니다.")
     @Min(value = 1, message = "평점은 1점 이상이어야 합니다.")
     @Max(value = 5, message = "평점은 5점 이하여야 합니다.")
@@ -20,6 +24,4 @@ public class ReviewCreateRequest {
     @Size(max = 50, message = "리뷰 내용은 50자 이하여야 합니다.")
     private String content;
 
-    @NotNull(message = "예약 ID는 필수입니다.")
-    private Long reservationId;
 }
