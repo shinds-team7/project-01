1d484029b9b287210dcd41ccaced39b038bb6ca5
hsbae <t01035724710@gmail.com>
2026-08-05 12:22:43 +0900

    feat: Reservation 전체 금액계산용 Mapper 함수 추가

--- src/main/java/com/example/petnow/mapper/PlaceMapper.java
+++ src/main/java/com/example/petnow/mapper/PlaceMapper.java
@@ -2,9 +2,11 @@ package com.example.petnow.mapper;
 
 import com.example.petnow.entity.Place;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface PlaceMapper {
     int insert(Place place);
 
+    Place findById(@Param("id") Long id);
 }
