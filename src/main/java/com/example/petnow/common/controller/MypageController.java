81b7df781455ff060bcafd5e07469f0bb040e4d2
rhkrqkq <rhkrqkq1025@gmail.com>
2026-08-05 14:11:04 +0900

    fix: 주석처리

--- src/main/java/com/example/petnow/common/controller/MypageController.java
+++ src/main/java/com/example/petnow/common/controller/MypageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MypageController {
 
-    @GetMapping("/mypage")
+    // @GetMapping("/mypage")
     public String mypage() {
         return "mypage";
     }
