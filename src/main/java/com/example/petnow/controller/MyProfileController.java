686a28c90d93d92951d63a0ee33e8ee967d596c5
seon0 <tjsdud00sy@gmail.com>
2026-08-06 17:27:08 +0900

    feat: 내 정보 상세 조회 Controller 구현

---
+++ src/main/java/com/example/petnow/controller/MyProfileController.java
@@ -0,0 +1,35 @@
+package com.example.petnow.controller;
+
+import com.example.petnow.common.constant.SessionConst;
+import com.example.petnow.dto.response.MyProfileResponse;
+import com.example.petnow.service.MyProfileServiceImpl;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/my")
+@RequiredArgsConstructor
+public class MyProfileController {
+
+    private final MyProfileServiceImpl myProfileServiceImpl;
+
+    @GetMapping("/profile")
+    public String getProgile(HttpSession session, Model model) {
+        Long userId = (Long) session.getAttribute(SessionConst.LOGIN_USER_ID);
+
+        if(userId == null) {
+            return "redirct:/auth/login";
+        }
+
+        MyProfileResponse profile = myProfileServiceImpl.getProfile(userId);
+
+        model.addAttribute("profile", profile);
+
+        // 페이지가 아직 없어, 페이지 생성 후 return값 변경할 예정입니다
+        return "index";
+    }
+}
