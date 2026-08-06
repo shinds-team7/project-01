6bfb87f9eae43aa87ce803deacab037a71df0a5f
seon0 <tjsdud00sy@gmail.com>
2026-08-06 17:22:40 +0900

    feat: 회원가입, 로그인 파일명 변경 user->auth

--- src/main/java/com/example/petnow/service/AuthService.java
+++ src/main/java/com/example/petnow/service/AuthService.java
@@ -3,9 +3,8 @@ package com.example.petnow.service;
 import com.example.petnow.dto.request.UserLoginRequest;
 import com.example.petnow.dto.request.UserSignupRequest;
 import com.example.petnow.dto.response.UserMyPageResponse;
-import com.example.petnow.entity.User;
 
-public interface UserService {
+public interface AuthService {
     void signup(UserSignupRequest request);
 
     Long login(UserLoginRequest request);
