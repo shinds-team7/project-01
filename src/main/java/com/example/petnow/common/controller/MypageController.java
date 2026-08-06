2876eef1e57143a2f31ba3a301a618cdbcd9683e
kowonyeong <ganenode@naver.com>
2026-08-06 12:36:39 +0900

    저장

--- src/main/java/com/example/petnow/common/controller/MypageController.java
+++ src/main/java/com/example/petnow/common/controller/MypageController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MypageController {
 
-    @GetMapping("/mypage")
-    public String mypage() {
-        return "mypage";
-    }
+//    @GetMapping("/mypage")
+//    public String mypage() {
+//        return "mypage";
+//    }
 }
