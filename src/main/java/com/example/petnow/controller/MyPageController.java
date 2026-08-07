2af8a7330d9a13ec4be5bebe8017dd99924492bb
seon0 <tjsdud00sy@gmail.com>
2026-08-07 10:32:06 +0900

    feat: 로그인 상태 확인

--- src/main/java/com/example/petnow/controller/MyPageController.java
+++ src/main/java/com/example/petnow/controller/MyPageController.java
@@ -24,6 +24,10 @@ public class MyPageController {
 
         Long userId = (Long) session.getAttribute(SessionConst.LOGIN_USER_ID);
 
+        if (userId == null) {
+            return "redirect:/auth/login";
+       }
+
         model.addAttribute("petList", petService.getPetList(userId));
 
         UserMyPageResponse user = authService.getMyPage(userId);
