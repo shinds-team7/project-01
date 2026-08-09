eb9e6cbeb9bbefcf45274fdad9a148c6682a2e5c
rhkrqkq <rhkrqkq1025@gmail.com>
2026-08-10 00:24:53 +0900

    feat: Reservation 에러 코드 정의

--- src/main/java/com/example/petnow/exception/ReservationErrorCode.java
+++ src/main/java/com/example/petnow/exception/ReservationErrorCode.java
@@ -14,7 +14,8 @@ public enum ReservationErrorCode implements ErrorCode {
 	PET_NOT_FOUND(HttpStatus.NOT_FOUND, "PET_NOT_FOUND","해당 반려동물을 찾을 수 없습니다."),
 	RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "RESERVATION_NOT_FOUND", "해당 예약을 찾을 수 없습니다."),
 	HOURLY_PRICE_NOT_SET(HttpStatus.BAD_REQUEST, "HOURLY_PRICE_NOT_SET", "시간당 가격이 설정되어 있지 않습니다."),
-	NIGHTLY_PRICE_NOT_SET(HttpStatus.BAD_REQUEST, "NIGHTLY_PRICE_NOT_SET", "1박당 가격이 설정되어 있지 않습니다.");
+	NIGHTLY_PRICE_NOT_SET(HttpStatus.BAD_REQUEST, "NIGHTLY_PRICE_NOT_SET", "1박당 가격이 설정되어 있지 않습니다."),
+	RESERVATION_ACCESS_DENIED(HttpStatus.FORBIDDEN, "RESERVATION_ACCESS_DENIED", "예약에 접근할 수 없습니다.");
 
 	private final HttpStatus status;
 	private final String code;
