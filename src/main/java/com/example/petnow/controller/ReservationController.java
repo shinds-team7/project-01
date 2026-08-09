10c30c8cea8c9f34be622dc7cca59f8928666c4d
rhkrqkq <rhkrqkq1025@gmail.com>
2026-08-09 15:59:57 +0900

    feat: 예약 취소 기능 구현

--- src/main/java/com/example/petnow/controller/ReservationController.java
+++ src/main/java/com/example/petnow/controller/ReservationController.java
@@ -6,17 +6,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.petnow.common.constant.SessionConst;
+import com.example.petnow.dto.request.ReservationCancelRequest;
 import com.example.petnow.dto.request.ReservationRequest;
 
 import com.example.petnow.dto.response.ReservationDetailResponse;
 import com.example.petnow.dto.response.ReservationListResponse;
-import com.example.petnow.mapper.ReservationMapper;
 import com.example.petnow.service.ReservationService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/reservation")
@@ -34,7 +38,8 @@ public class ReservationController {
 	}
 
 	@GetMapping("/detail")
-	public String detailReservation(@ModelAttribute ReservationDetailResponse response, @RequestParam Long reservationId, @RequestParam Long userId,
+	public String detailReservation(@ModelAttribute ReservationDetailResponse response,
+		@RequestParam Long reservationId, @RequestParam Long userId,
 		Model model) {
 		ReservationDetailResponse reservation = reservationService.detailReservation(response.getReservationId());
 		model.addAttribute("reservation", reservation);
@@ -47,5 +52,15 @@ public class ReservationController {
 		List<ReservationListResponse> responseList = reservationService.getReservationList(userId, useStatus);
 		model.addAttribute("reservations", responseList);
 		return "reservations/reservationList";
+	}
+
+	@PostMapping("/cancel")
+	public String cancel(@ModelAttribute ReservationCancelRequest request, HttpSession session) {
+		Long userId = (Long)session.getAttribute(SessionConst.LOGIN_USER_ID);
+		if (userId == null) {
+			return "redirect:/";
+		}
+		reservationService.cancelReservation(request.getReservationId(), userId);
+		return "redirect:/reservation/list?userId=" + userId;
 	}
 }
