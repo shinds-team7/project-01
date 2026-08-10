57bf534bef2a9534c681729ac95544065d3df191
rhkrqkq <rhkrqkq1025@gmail.com>
2026-08-10 16:44:14 +0900

    feat: 예약 에러코드 추가

--- src/main/java/com/example/petnow/exception/ReservationErrorCode.java
+++ src/main/java/com/example/petnow/exception/ReservationErrorCode.java
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 public enum ReservationErrorCode implements ErrorCode {
 
 	INVALID_RESERVATION_PERIOD(HttpStatus.BAD_REQUEST, "INVALID_RESERVATION_PERIOD", "체크인/체크아웃 날짜가 올바르지 않습니다."),
+	INVALID_RESERVATION_STATUS(HttpStatus.BAD_REQUEST, "INVALID_RESERVATION_STATUS", "처리할 수 없는 예약 상태입니다."),
+	RESERVATION_UPDATE_FAILED(HttpStatus.BAD_REQUEST, "RESERVATION_UPDATE_FAILED", "상태 업데이트에 실패했습니다."),
 	RESERVATION_TOO_SHORT(HttpStatus.BAD_REQUEST, "RESERVATION_TOO_SHORT", "예약 시간은 최소 1시간 이상입니다."),
 	PET_NOT_FOUND(HttpStatus.NOT_FOUND, "PET_NOT_FOUND","해당 반려동물을 찾을 수 없습니다."),
 	RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "RESERVATION_NOT_FOUND", "해당 예약을 찾을 수 없습니다."),
