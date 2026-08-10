666080f99a16c80853e20e0677b08baa79f1d091
seon0 <tjsdud00sy@gmail.com>
2026-08-10 15:29:43 +0900

    feat: 회원 탈퇴 Controller 추가

--- src/main/java/com/example/petnow/controller/MyProfileController.java
+++ src/main/java/com/example/petnow/controller/MyProfileController.java
@@ -1,13 +1,16 @@
 package com.example.petnow.controller;
 
 import com.example.petnow.common.constant.SessionConst;
+import com.example.petnow.common.session.LoginSession;
 import com.example.petnow.dto.response.MyProfileResponse;
+import com.example.petnow.service.MyProfileService;
 import com.example.petnow.service.MyProfileServiceImpl;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,8 +19,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MyProfileController {
 
     private final MyProfileServiceImpl myProfileServiceImpl;
+    private final MyProfileService myProfileService;
 
     @GetMapping("/profile")
+    // 프로필 상세 조회
     public String getProgile(HttpSession session, Model model) {
         Long userId = (Long) session.getAttribute(SessionConst.LOGIN_USER_ID);
 
@@ -32,4 +37,19 @@ public class MyProfileController {
         // 페이지가 아직 없어, 페이지 생성 후 return값 변경할 예정입니다
         return "mypage/myprofile-test";
     }
+
+
+    // 회원탈퇴 , 페이지 생성 후 버튼, form 연결 필요
+    @PostMapping("/withdraw")
+    public String withdrqw(HttpSession session) {
+
+        Long userId = (Long) session.getAttribute(SessionConst.LOGIN_USER_ID);
+
+        myProfileService.withdraw(userId);
+
+        LoginSession.clear(session);
+
+        return "redirect:/home";
+    }
+
 }
