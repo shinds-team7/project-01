4afb8ca306ac403c906905cbb93a051da6f050bf
rhkrqkq <rhkrqkq1025@gmail.com>
2026-08-10 10:08:48 +0900

    fix: 오류코드 오탈자 수정

--- src/main/java/com/example/petnow/exception/ReservationErrorCode.java
+++ src/main/java/com/example/petnow/exception/ReservationErrorCode.java
@@ -11,7 +11,7 @@ public enum ReservationErrorCode implements ErrorCode {
 
 	INVALID_RESERVATION_PERIOD(HttpStatus.BAD_REQUEST, "INVALID_RESERVATION_PERIOD", "체크인/체크아웃 날짜가 올바르지 않습니다."),
 	PET_NOT_FOUND(HttpStatus.NOT_FOUND, "PET_NOT_FOUND","해당 반려동물을 찾을 수 없습니다."),
-	RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "RESERVATION_NOT_FOUND", "해당 예약을 찾을 수 없습니디ㅏ.");
+	RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "RESERVATION_NOT_FOUND", "해당 예약을 찾을 수 없습니다.");
 
 	private final HttpStatus status;
 	private final String code;
