a404fe393bf5af90d7f79ad4f0ecec2aefc37ca9
rhkrqkq <rhkrqkq1025@gmail.com>
2026-08-10 09:44:59 +0900

    refactor: reservationId @NotNull 추가

--- src/main/java/com/example/petnow/dto/request/ReservationCancelRequest.java
+++ src/main/java/com/example/petnow/dto/request/ReservationCancelRequest.java
@@ -1,5 +1,6 @@
 package com.example.petnow.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,5 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReservationCancelRequest {
+
+	@NotNull
 	private Long reservationId;
 }
