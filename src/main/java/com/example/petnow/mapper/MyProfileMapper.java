67331804349c071fab7b903db28538ed723a14ce
seon0 <tjsdud00sy@gmail.com>
2026-08-11 12:29:20 +0900

    fix: findById 메서드 역할 분리 주석 추가

--- src/main/java/com/example/petnow/mapper/MyProfileMapper.java
+++ src/main/java/com/example/petnow/mapper/MyProfileMapper.java
@@ -5,6 +5,8 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MyProfileMapper {
+
+    // 내 정보 상세 조회를 위한 메서드
     User findById(Long userId);
 
     int withdraw(Long userId);
