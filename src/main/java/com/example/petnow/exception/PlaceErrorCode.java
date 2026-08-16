f752d06be764ae34c75a1bcd618371935bad12f0
현식 배 <t01035724710@gmail.com>
2026-08-16 09:13:10 +0900

    [feat]: Place 슬롯과 관련된 에러코드추가

--- src/main/java/com/example/petnow/exception/PlaceErrorCode.java
+++ src/main/java/com/example/petnow/exception/PlaceErrorCode.java
@@ -18,6 +18,36 @@ public enum PlaceErrorCode implements ErrorCode {
             HttpStatus.INTERNAL_SERVER_ERROR,
             "PLACE_CREATE_FAILED",
             "장소 등록에 실패했습니다."
+    ),
+
+    PLACE_ACCESS_DENIED(
+            HttpStatus.FORBIDDEN,
+            "PLACE_ACCESS_DENIED",
+            "해당 장소를 관리할 권한이 없습니다"
+    ),
+
+    PLACE_AVAILABILITY_PERIOD_INVALID(
+            HttpStatus.BAD_REQUEST,
+            "PLACE_AVAILABILITY_PERIOD_INVALID",
+            "슬롯 생성 기간이 올바르지 않습니다"
+    ),
+
+    PLACE_SLOT_STATUS_INVALID(
+            HttpStatus.BAD_REQUEST,
+            "PLACE_SLOT_STATUS_INVALID",
+            "변경할 수 없는 슬롯 상태입니다"
+    ),
+
+    PLACE_SLOT_UPDATE_FAILED(
+            HttpStatus.BAD_REQUEST,
+            "PLACE_SLOT_UPDATE_FAILED",
+            "슬롯 상태를 변경할 수 없습니다"
+    ),
+
+    PLACE_PACKAGE_TIME_INVALID(
+            HttpStatus.BAD_REQUEST,
+            "PLACE_PACKAGE_TIME_INVALID",
+            "패키지 입실과 퇴실 시각은 3시간 격자에 맞아야 합니다"
     );
 
     private final HttpStatus status;
